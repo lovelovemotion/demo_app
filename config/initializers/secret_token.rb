7e3161fcbd867900c7412e9115bec34053165dad
lovelovemotion <lovelovemotion@gmail.com>
2013-03-20 23:11:19 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '167ee0b796cf1e2bd585184eb37d39faf69fd56afcfdd9abf170e6a3fbc39cc404e8f7f97e3c7908b07c1ce8ef827d40e829b4643da72d931c52a2dfccf361ef'
